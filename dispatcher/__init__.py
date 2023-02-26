3ca3f85d11899f3eca8100f695c483eee8abf40f
Martell <vovasamb@yandex.ru>
2023-02-26 20:09:42 +0300

    File optimisation

--- dispatcher/__init__.py
+++ dispatcher/__init__.py
@@ -1,3 +1,6 @@
-from dispatcher import bot_init
+from aiogram import Bot, Dispatcher
 
-dp = bot_init.dp
+API_TOKEN = '6203341812:AAFuPdsP-g0kjW0XkiNRUxjsn6EJ1hwLY-8'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
